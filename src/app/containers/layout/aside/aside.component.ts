47416dfbaba52d0723365b8f64ff8c3233780828
Matheus Souza <joaomtfslp123@gmail.com>
2024-04-14 13:33:01 -0400

    Atualização

--- src/app/containers/layout/aside/aside.component.ts
+++ src/app/containers/layout/aside/aside.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+declare const $: any;
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
 export class AsideComponent {
+  menuIsOpen = false;
+  constructor() { }
 
+  changeMenuState(isCompoustMenu: boolean) {
+    console.log('changeMenuState', isCompoustMenu);
+    if (this.menuIsOpen) {
+      setTimeout(() => {
+        $('.collapse').collapse('hide');
+      }, 300);
+
+      this.menuIsOpen = !this.menuIsOpen;
+    } else if (isCompoustMenu) {
+      this.menuIsOpen = !this.menuIsOpen;
+    }
+  }
 }
