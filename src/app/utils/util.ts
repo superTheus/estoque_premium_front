1168ec9b9548d73e85f05ca45631e0d2c5803075
Matheus Souza <joaomtfslp123@gmail.com>
2024-04-29 22:45:58 -0400

    atualização

--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -24,7 +24,7 @@ import { User } from "../shared/auth.interface";
 export const getCompanyId = () => {
   let id = 0
   try {
-    id = Number(sessionStorage.getItem('company_login_id')) || 0;
+    id = Number(localStorage.getItem('company_login_id')) || 0;
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : getUserId -> error", error)
     id = 0;
@@ -34,7 +34,7 @@ export const getCompanyId = () => {
 
 export const setCompanyId = (userId: number) => {
   try {
-    sessionStorage.setItem('company_login_id', String(userId));
+    localStorage.setItem('company_login_id', String(userId));
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : setUserId -> error")
   }
@@ -43,7 +43,7 @@ export const setCompanyId = (userId: number) => {
 export const getUserId = () => {
   let id = 0
   try {
-    id = Number(sessionStorage.getItem('user_login_id')) || 0;
+    id = Number(localStorage.getItem('user_login_id')) || 0;
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : getUserId -> error", error)
     id = 0;
@@ -53,7 +53,7 @@ export const getUserId = () => {
 
 export const setUserId = (userId: number) => {
   try {
-    sessionStorage.setItem('user_login_id', String(userId));
+    localStorage.setItem('user_login_id', String(userId));
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : setUserId -> error")
   }
@@ -62,7 +62,7 @@ export const setUserId = (userId: number) => {
 export const getUserEmail = () => {
   let token = '';
   try {
-    token = sessionStorage.getItem('user_login_email') || '';
+    token = localStorage.getItem('user_login_email') || '';
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : getUserEmail -> error", error)
     token = '';
@@ -72,7 +72,7 @@ export const getUserEmail = () => {
 
 export const setUserEmail = (email: string) => {
   try {
-    sessionStorage.setItem('user_login_email', email);
+    localStorage.setItem('user_login_email', email);
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : setUserEmail -> error")
   }
@@ -80,7 +80,7 @@ export const setUserEmail = (email: string) => {
 
 export const setUser = (user: User) => {
   try {
-    sessionStorage.setItem('user_data', JSON.stringify(user));
+    localStorage.setItem('user_data', JSON.stringify(user));
   } catch (error) {
     console.log(">>>> src/app/utils/util.js : setUser -> error")
   }
@@ -88,7 +88,7 @@ export const setUser = (user: User) => {
 
 export const getUser = () => {
   try {
-    let token = sessionStorage.getItem('user_data');
+    let token = localStorage.getItem('user_data');
     if (token)
       return JSON.parse(token);
     else
@@ -100,7 +100,6 @@ export const getUser = () => {
 }
 
 export const clearSession = () => {
-  sessionStorage.clear();
   localStorage.clear();
 }
 
